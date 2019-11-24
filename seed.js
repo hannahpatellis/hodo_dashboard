f706b9533fc158e43ce848ce658bf87a49a968f3
Hannah Patellis <hpatellis@icloud.com>
2019-11-23 21:24:33 -0500

    Updates seed script to include challenges. Renames images to fix case-sensitivity

--- seed.js
+++ seed.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const db = require('./server/models/index.js');
 
+const houseSeed = require('./seed/houseSeed.json');
+const challengeSeed = require('./seed/challengeSeed.json');
+
 require('dotenv').config();
 
 const mongouri = process.env.MONGODB_URI || 'mongodb://localhost/hododash';
@@ -13,46 +16,26 @@ mongoose.connection.on('error', (err) => {
 	process.exit(1);
 });
 
-const houseSeed = [
-  {
-    'house': 'Gestalt',
-    'head': 'Dishanta',
-    'points': 0,
-    'weekpoints': 0,
-    'owl': false,
-    'image': 'gestalt.png',
-    'hex': 'D54D49'
-  },
-  {
-    'house': 'Context',
-    'head': 'Adriana',
-    'points': 0,
-    'weekpoints': 0,
-    'owl': false,
-    'image': 'context.png',
-    'hex': '7A52E0'
-  },
-  {
-    'house': 'Heuristics',
-    'head': 'Devin',
-    'points': 0,
-    'weekpoints': 0,
-    'owl': false,
-    'image': 'heuristics.png',
-    'hex': '009478'
-  }
-];
-
 db.House.create(houseSeed)
   .then(result => {
-    console.log('Success!');
+    console.log('House Seed: Success!');
     console.log('------------');
     console.log(result);
-    process.exit(1);
   })
   .catch(err => {
-    console.log('There\'s been an error');
+    console.log('House Seed: There\'s been an error');
     console.log('------------');
     console.log(err);
-    process.exit(1);
+  });
+
+db.Challenge.create(challengeSeed)
+  .then(result => {
+    console.log('Challenge Seed: Success!');
+    console.log('------------');
+    console.log(result);
+  })
+  .catch(err => {
+    console.log('Challenge Seed: There\'s been an error');
+    console.log('------------');
+    console.log(err);
   });
